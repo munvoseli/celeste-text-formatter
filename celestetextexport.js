fb55bbddcb90a14ea5d03ee368727ff87046f450
Melody <60828599+munvoseli@users.noreply.github.com>
2021-01-10 00:47:03 -0500

    Add files via upload

---
+++ celestetextexport.js
@@ -0,0 +1,211 @@
+var fileInput = document.createElement("input");
+fileInput.setAttribute("type", "file");
+fileInput.multiple = true;
+document.body.appendChild(fileInput);
+var a = document.createElement("a");
+document.body.appendChild(a);
+function setUpDownload(u8arr, nom)
+{
+    a.href = URL.createObjectURL(new Blob([u8arr], {type: "application/octet-stream"}));
+    a.download = nom;
+    a.innerHTML = Math.random();
+}
+function getvarlenrep (n) // little endian
+{
+    const len = Math.ceil(Math.log2(n + 1) / 7);
+    var arr = new Uint8Array(len);
+    arr[0] = n & 127;
+    n >>= 7;
+    for (var i = 1; i < len; i++)
+    {
+	var a  = n & 127;
+	arr[i] = a | 128;
+	n >>= 7;
+    }
+    return arr;
+}
+
+var encoder = new TextEncoder();
+function getStringThing(str)
+{
+    // I have no idea what number format this is
+    // It's not MIDI-style variable length
+    // If you know, please tell
+    var n = encoder.encode(str).length;
+    if (n < 128)
+    {
+	return new Uint8Array([n]);
+    }
+    else
+    {
+	var byte0 = n & 255;
+	var byte1 = n >> 7;
+	byte1 &= 254 | (byte0 >> 7);
+	byte0 |= 128;
+	return new Uint8Array([byte0, byte1]);
+    }
+}
+function putStringInUint (arr, string, i)
+{
+    var n = getStringThing (string);
+    for (var j = 0; j < n.length; j++)
+	arr[i++] = n[j];
+    var u8arr = encoder.encode(string);
+    for (var j = 0; j < u8arr.length; j++)
+	arr[i++] = u8arr[j];
+    return n.length + u8arr.length;
+}
+
+function exportString(string)
+{
+    var arr = new Uint8Array(2 * string.length); // just a guess, might actually not be enough. If it's not, you'll see that the console screams "AAAAAAH"
+    var header = "\x07english\x07English\x1aIcons/canadian-english.png\x00\x00\x00\x00\x08Renogare\x00\x00\x80B\x0a(\\s|\\{|\\})\x01,\x03.!?\xdf\x08\x00\x00\x8f-\x00\x00\x13\x02\x00\x00"; // just for English
+    var i = 0;
+    // curse the lack of polish notation / rpn
+    while (i < header.length)
+	arr[i] = header.charCodeAt(i++);
+    string = string.replace(/\r/g, "");
+    string = string.replace(/\n[\t ]+/g, "\n");
+    string = string.replace(/[\t ]+\n/g, "\n");
+    string = string.replace(/\b=\s*/g, "=\n"); // \b matches the beginning or end of a words, and is used here to prevent the file namer keyboard thing from being read as a key
+    string = string.replace(/\n#.*/g, "");
+    string = string.replace(/^#.*/, "");
+    string += "\na="; // to combat infix notation
+    var pre = document.createElement("pre");
+    pre.innerHTML = (string);
+    document.body.appendChild(pre);
+    var j = string.search("BEGIN");
+    var jafterspace = j;
+    var jequals = -1;
+    var key, value;
+    const KVARR_STEP = 2;
+    var kvarr = [];
+    var usedMacros = string.match(/\{\+.*?\}/g);
+    var uniqueMacros = usedMacros.filter((c, index) => { // this function brought to you by the internet
+	return usedMacros.indexOf(c) === index;
+    });
+    var macroTable = []; // [index, macro]
+    for (var macro of uniqueMacros)
+    {
+	macroTable.push([-1, macro]);
+    }
+    // curse infix notation. RPN FTW
+    while (j < string.length)
+    {
+	if (string[j] == "=" && string[j - 1] != "+") // the string[j - 1] != "+" is to prevent the file name thing from being read as a key
+	{
+	    if (jequals != -1)
+	    {
+		//value = string.substring(jequals + 2, jafterspace - 1).replace(/\{n\}\n+/g, "{n}").replace(/\n+\{n\}/g, "{n}");
+		value = string.substring(jequals + 2, jafterspace - 1).replace(/\{n\}\n+/g, "{n}").replace(/\n+$/, "");
+		if (jequals + 2 > jafterspace - 1) // sometimes, there'll be an equation sign where there's nothing after it and that's represented by two 00 bytes and no numerical bytes
+		    value = "";
+		kvarr.push(key);
+		kvarr.push(value);
+	    }
+	    key = string.substring(jafterspace, j);
+	    jequals = j;
+	}
+	else if (string[j] != "\n" && string[j - 1] == "\n")
+	{
+	    jafterspace = j;
+	}
+	j++;
+    }
+    
+    // done processing the string, now we have an array that we need to process more
+    for (var j = 0; j < kvarr.length; j += KVARR_STEP) // key, valuestrict, valuesimple
+    {
+	for (var indmacro of macroTable)
+	{
+	    if (indmacro[1].toLowerCase() == "{+" + kvarr[j].toLowerCase() + "}" ||
+		indmacro[1].toLowerCase() == "{+ " + kvarr[j].toLowerCase() + "}")
+	    {
+		indmacro[0] = j + 1;
+	    }
+	}
+    }
+
+ 
+    for (var j = 0; j < kvarr.length; j += KVARR_STEP) // split the lines
+	if (kvarr[j + 1])
+	    kvarr[j + 1] = kvarr[j + 1].split(/\n+/);
+
+    for (var j = 0; j < kvarr.length; j += KVARR_STEP)
+    {
+	if (kvarr[j + 1] === "")
+	    kvarr[j + 1] = [];
+    }
+    // if the line is a text line and not a trigger/silent_trigger or portrait line, add a break after
+    for (var j = 0; j < kvarr.length; j += KVARR_STEP)
+    {
+	var block = kvarr[j + 1];
+	for (var l = 0; l < block.length - 1; l++)
+	{
+	    var ffour = block[l].substring(0, 4);
+	    // anchor, silent_trigger, trigger, portrait
+	    if (ffour != "{anc" && ffour != "{sil" && block[l].substring(0, 4) != "{tri" && block[l][0] != "[" && ffour != "{por")
+		block[l] += "{break}";
+	}
+    }
+    for (var j = 0; j < kvarr.length; j += KVARR_STEP)
+    {
+	if (!kvarr[j + 1])
+	    console.log(kvarr[j]);
+	kvarr[j + 1] = kvarr[j + 1].join("").replace(/\n+/g, "{break}").replace(/\[/g, "{portrait ").replace(/\]/g, "}");
+    }
+
+    var hasReplaced = true;
+    //for (var asdf = 0; asdf < 2; asdf++) // for another age
+    while (hasReplaced) // use the macros luke
+    {
+	hasReplaced = false;
+	for (var j = 0; j < kvarr.length; j += KVARR_STEP)
+	{
+	    for (var indmacro of macroTable)
+	    {
+		if (kvarr[j + 1].indexOf(indmacro[1]) >= 0)
+		{
+		    hasReplaced = true;
+		    kvarr[j + 1] = kvarr[j + 1].replaceAll(indmacro[1], kvarr[indmacro[0]]).replaceAll("\\#", "#");
+		}
+	    }
+	}
+    }
+    console.log(macroTable);
+    for (var j = 0; j < kvarr.length; j += KVARR_STEP)
+    {
+	if (kvarr[j + 1])
+	{
+	    i += putStringInUint(arr, kvarr[j    ], i); // key
+	    i += putStringInUint(arr, kvarr[j + 1], i); // value full
+	    var simp = kvarr[j + 1].replaceAll("{n}", "\n").replaceAll("{break}", "\n").replace(/\{.*?\}/g, ""); // squirrel in my pants
+	    i += putStringInUint(arr, simp, i); // value simple
+	}
+	else // remember that two 00 byte thing from earlier?
+	{
+	    i += putStringInUint(arr, kvarr[j    ], i);
+	    i += 2;
+	}
+    }
+    pre.innerHTML = JSON.stringify(kvarr, null, 4);
+    if (i >= arr.length)
+	console.error("aah");
+    return arr.slice(0, i);
+}
+
+function loadFile(file) {
+    var fileReader = new FileReader();
+    fileReader.addEventListener("loadend", function(e) {
+	var result = fileReader.result;
+	var u8arr = exportString(result);
+	setUpDownload(u8arr, file.name + ".export");
+    }, false);
+    fileReader.readAsText(file);
+}
+fileInput.addEventListener("change", function() {
+    var files = fileInput.files;
+    for (var file of files) {
+	loadFile(file);
+    }
+}, false);
